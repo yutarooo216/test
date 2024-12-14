5756a02dd72429acb34c7035b764fce15c494477
yutarooo216 <yutarooo216@gmail.com>
2024-12-14 09:54:14 +0900

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,17 @@
+# backend
+terraform {
+  backend "s3" {
+    bucket = "cicd-bucket-yutarooo216"
+    key = "test/resouces"
+    region = "ap-northeast-1"
+    encrypt = true
+  }
+}
+
+# VPC
 resource "aws_vpc" "main" {
-	cidr_block = "10.0.0.0/16"
-	tags = {
-		Name = "githubactions-vpc"
-	}
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "githubactions-vpc"
+  }
 }
